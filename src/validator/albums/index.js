b0ee0e5229a9f3941536c5470c70a8f7d5b9edec
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-13 17:45:12 +0700

    create exceptions

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,10 +1,11 @@
+const InvariantError = require('../../exceptions/InvariantError');
 const {AlbumPayloadSchema} = require('./schema');
 
 const AlbumValidator = {
   validateAlbumPayload: (payload) => {
     const validationResult = AlbumPayloadSchema.validate(payload);
     if (validationResult.error) {
-      throw new Error(validationResult.error.message);
+      throw new InvariantError(validationResult.error.message);
     }
   },
 };
