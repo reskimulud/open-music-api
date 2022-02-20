d53d2a5a7bcd3defd96c51289498cc2626d6811b
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-20 21:24:46 +0700

    [RE-INSTALL NPM] create users service

---
+++ src/services/UsersService.js
@@ -0,0 +1,82 @@
+const {nanoid} = require('nanoid');
+const {Pool} = require('pg');
+const InvariantError = require('../exceptions/InvariantError');
+const bcrypt = require('bcrypt');
+const AuthenticationError = require('../exceptions/AuthenticationError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({username, password, fullname}) {
+    await this.verifyNewUsername(username);
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new InvariantError('User was not added');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount > 0) {
+      throw new InvariantError('User was not added, username has been used');
+    }
+  }
+
+  async getUserById(id) {
+    const query = {
+      text: 'SELECT * FROM users WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new InvariantError('User was not found');
+    }
+
+    return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT username, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new AuthenticationError('User was not found');
+    }
+
+    const {id, password: hashedPassword} = result.rows[0];
+
+    const isValid = await bcrypt.compare(password, hashedPassword);
+
+    if (!isValid) {
+      throw new AuthenticationError('User was not found');
+    }
+
+    return id;
+  }
+}
+
+module.exports = UsersService;
