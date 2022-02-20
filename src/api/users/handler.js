5539e94dc1461146900af8fb9886d38a3c9a900f
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-20 22:17:35 +0700

    create users plugin

---
+++ src/api/users/handler.js
@@ -0,0 +1,78 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
+
+  async postUserHandler(request, h) {
+    try {
+      this._validator.validateUserPayload(request.payload);
+      const {username, password, fullname} = request.payload;
+
+      const userId = await this._service.addUser({
+        username,
+        password,
+        fullname});
+
+      const response = h.response({
+        status: 'success',
+        message: 'User added',
+        data: {
+          userId,
+        },
+      });
+      response.code(2001);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+
+  async getUserByIdHandler(request, h) {
+    try {
+      const {id} = request.params;
+      const user = await this._service.getUserById(id);
+
+      return {
+        status: 'success',
+        data: {
+          user,
+        },
+      };
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
