d021975dfb52b6615b80624fe296a55a75790cc7
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-21 03:28:16 +0700

    [RE-INSTALL NPM] create token manager (JWT)

---
+++ src/tokenize/TokenManager.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+const Jwt = require('@hapi/jwt');
+const InvariantError = require('../exceptions/InvariantError');
+
+const TokenManager = {
+  generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
+  generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+  verifyRefreshToken: (refreshToken) => {
+    try {
+      const artifacts = Jwt.token.decode(refreshToken);
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+
+      const {payload} = artifacts.decoded;
+      return payload;
+    } catch (err) {
+      throw new InvariantError('Refresh token was not valid');
+    }
+  },
+};
+
+module.exports = TokenManager;
