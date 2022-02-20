a74d5b19701f0d566f907afb8f825970a35c824b
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-21 06:01:43 +0700

    create authentications service

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,128 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+
+    this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this);
+    this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
+    // eslint-disable-next-line max-len
+    this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
+  }
+
+  async postAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePostAuthenticationPayload(request.payload);
+
+      const {username, password} = request.payload;
+      const id = await this._usersService.verifyCredential(username, password);
+
+      const accessToken = this._tokenManager.generateAccessToken({id});
+      const refreshToken = this._tokenManager.generateRefreshToken({id});
+
+      await this._authenticationsService.addRefreshToken(refreshToken);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Authentication added',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(err);
+      return response;
+    }
+  }
+
+  async putAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePutAuthenticationPayload(request.payload);
+
+      const {refreshToken} = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      const {id} = this._tokenManager.verifyRefreshToken(refreshToken);
+
+      const accessToken = await this._tokenManager.generateAccessToken({id});
+
+      return {
+        status: 'success',
+        message: 'Access token updated',
+        data: {
+          accessToken,
+        },
+      };
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(err);
+      return response;
+    }
+  }
+
+  async deleteAuthenticationHandler(request, h) {
+    try {
+      this._validator.validateDeleteAuthenticationPayload(request.payload);
+
+      const {refreshToken} = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+      return {
+        status: 'success',
+        message: 'Refresh token deleted',
+      };
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(err);
+      return response;
+    }
+  }
+}
+
+module.exports = AuthenticationsHandler;
