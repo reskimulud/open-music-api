6cc296dd25d92b7177e897fea7468ef3f461aaf7
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-23 11:06:49 +0700

    create collaborations plugin

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,88 @@
+/* eslint-disable max-len */
+const ClientError = require('../../exceptions/ClientError');
+
+class CollaborationsHandler {
+  constructor(collaborationsService, playlistsService, validator) {
+    this._collaborationsService = collaborationsService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+
+    this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const {id: credentialId} = request.auth.credentials;
+      const {playlistId, userId} = request.payload;
+
+      await this._playlistsService.verifyOwner(playlistId, credentialId);
+      const collaborationId = await this._collaborationsService.addCollaboration(playlistId, userId);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Collaboration added',
+        data: {
+          collaborationId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal Server Error',
+      });
+      response.code(500);
+      console.error(err);
+      return response;
+    }
+  }
+
+  async deleteCollaborationHandler(request, h) {
+    try {
+      this._validator.validateDeleteCollaborationPayload(request.payload);
+      const {id: credentialId} = request.auth.credentials;
+      const {collaborationId} = request.payload;
+
+      await this._collaborationsService.verifyOwner(collaborationId, credentialId);
+      await this._collaborationsService.deleteCollaboration(collaborationId);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Collaboration removed',
+      });
+      response.code(200);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal Server Error',
+      });
+      response.code(500);
+      console.error(err);
+      return response;
+    }
+  }
+}
+
+module.exports = CollaborationsHandler;
