dc580b5615f3532fc960182d8e1c9e5e6a5ade5d
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-24 04:54:06 +0700

    create migration to add FK album_id column

---
+++ migrations/1645652938668_add-foreign-key-to-album-id-column-at-songs-table.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  // add foreign key to album_id column at songs table
+  pgm.addConstraint(
+      'songs',
+      'fk_songs.album_id_albums.id',
+      'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  // drop foreign key from album_id column at songs table
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
+};
